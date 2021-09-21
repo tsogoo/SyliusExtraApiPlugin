6f42ab45bfb5f77ad145ab6cc9a3091d9e071f0a
Tsogoo <ts.r.madrid@gmail.com>
2021-09-22 00:41:48 +0800

    boxed

---
+++ src/Controller/Api/DeleteOrderItemAction.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace DavidRoberto\SyliusExtraApiPlugin\Controller\Api;
+
+use Sylius\Bundle\ApiBundle\Controller;
+
+use Sylius\Bundle\ApiBundle\Command\Cart\RemoveItemFromCart;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+
+
+
+final class DeleteOrderItemAction
+{
+    /** @var MessageBusInterface */
+    private $commandBus;
+
+    public function __construct(MessageBusInterface $commandBus, OrderRepositoryInterface $orderRepository)
+    {
+        $this->commandBus = $commandBus;
+        $this->orderRepository = $orderRepository;
+    }
+
+    public function __invoke(Request $request)
+    {
+        $command = new RemoveItemFromCart(
+            $request->attributes->get('tokenValue'),
+            $request->attributes->get('itemId')
+        );
+        $this->commandBus->dispatch($command);
+        return $this->orderRepository->findCartByTokenValue($request->attributes->get('tokenValue'));
+    }
+}
